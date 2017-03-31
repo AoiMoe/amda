68b3519f1280b48d6f1b05c09d6e648b3a76b082
Takuya SHIOZAKI <tshiozak@bsdclub.org>
2017-03-31 20:09:04 +0900

    change copyright year.

--- amda.h
+++ amda.h
@@ -1,5 +1,5 @@
 //
-// Copyright (c)2008,2016 Takuya SHIOZAKI,
+// Copyright (c)2017 Takuya SHIOZAKI,
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
